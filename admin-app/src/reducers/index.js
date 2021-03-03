23f7839d934674db392a71a0c32714c93d76ea70
Ajay Kumar <neseajaykumar9347@gmail.com>
2021-03-03 10:45:40 +0530

    admin-source

---
+++ admin-app/src/reducers/index.js
@@ -0,0 +1,21 @@
+import authReducer from './auth.reducers';
+import userReducer from './user.reducers';
+import  productReducer  from "./product.reducer";
+import  categoryReducer from './category.reducer';
+import  orderReducer  from "./order.reducer";
+import { combineReducers} from 'redux';
+import pageReducer from './page.reducer';
+
+
+
+const rootReducer = combineReducers({
+    auth: authReducer,
+    user: userReducer,
+    category :categoryReducer,
+    product: productReducer,
+    order: orderReducer,
+    page: pageReducer
+});
+
+
+export default rootReducer;
